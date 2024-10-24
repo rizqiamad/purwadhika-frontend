7386be468a82d0b9423b38f176ddefa44428d699
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-10-24 11:28:57 +0700

    chakra UI

---
+++ chakra-react/src/components/ui/myComp/accordion.tsx
@@ -0,0 +1,57 @@
+import { Badge, HStack } from "@chakra-ui/react"
+import {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionRoot,
+} from "@/components/ui/accordion"
+import { Avatar } from "@/components/ui/avatar"
+import { LuTrophy } from "react-icons/lu"
+// import { LoremIpsum } from "react-lorem-ipsum"
+
+const MyAccordion = () => {
+  return (
+    <AccordionRoot collapsible defaultValue={["b"]}>
+      {items.map((item, index) => (
+        <AccordionItem key={index} value={item.name}>
+          <AccordionItemTrigger>
+            <Avatar shape="rounded" src={item.image} name={item.name} />
+            <HStack>
+              {item.name}{" "}
+              {item.topRated && (
+                <Badge colorPalette="green">
+                  <LuTrophy />
+                  Top Rated
+                </Badge>
+              )}
+            </HStack>
+          </AccordionItemTrigger>
+          <AccordionItemContent>{item.bio}</AccordionItemContent>
+        </AccordionItem>
+      ))}
+    </AccordionRoot>
+  )
+}
+
+const items = [
+  {
+    name: "Alex",
+    bio: '<LoremIpsum />',
+    image: "https://i.pravatar.cc/150?u=a",
+    topRated: false,
+  },
+  {
+    name: "Benji",
+    bio: '<LoremIpsum />',
+    image: "https://i.pravatar.cc/150?u=b",
+    topRated: true,
+  },
+  {
+    name: "Charlie",
+    bio: '<LoremIpsum />',
+    image: "https://i.pravatar.cc/150?u=c",
+    topRated: false,
+  },
+]
+
+export default MyAccordion
