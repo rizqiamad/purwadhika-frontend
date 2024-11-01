ac9061ebbda38ae885a87d125f7a280b81ad271a
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-11-01 13:55:49 +0700

    params debugging

--- next-app/src/app/(home)/user/[userId]/page.tsx
+++ next-app/src/app/(home)/user/[userId]/page.tsx
@@ -1,7 +1,8 @@
-export default function UserDetailPage({ params }: { params: { userId: string } }) {
+export default async function UserDetailPage({ params }: { params: Promise<{ userId: string }> }) {
+  const { userId } = await params;
   return (
     <div>
-      User Id : {params.userId}
+      User Id : {userId}
     </div>
   )
 }
--- twitter-react/src/components/protectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-import { Navigate, Outlet } from "react-router-dom"
-
-export default function Protected() {
-  const [userId] = useState<string | null>(localStorage.getItem('userId') || null)
-  return (
-    userId ? <Outlet /> : <Navigate to={'/'}/>
-  )
-}
