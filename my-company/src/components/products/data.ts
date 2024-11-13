ba5aa0009ea2eca1879dca1fb171a4d4a07bb090
Hanif Ahmad <ahmadhanif759@gmail.com>
2024-11-13 22:16:55 +0700

    product data.ts

--- my-company/src/components/products/data.ts
+++ my-company/src/components/products/data.ts
@@ -1,55 +1,55 @@
 export const dataDetailedPage = [
   {
-    title: "Laptop",
+    title: "Laptops",
     name: "Caca",
     review:
       "Saya baru saja membeli laptop ROG dan saya sangat terkesan dengan performanya! Laptop ini memiliki kecepatan luar biasa, bahkan untuk game berat sekalipun. Desainnya keren dan tampak premium, dan saya bisa mengandalkannya untuk kerja berat sehari-hari. Dengan sistem pendingin yang efektif, saya bisa bermain game selama berjam-jam tanpa khawatir laptop panas. Benar-benar investasi yang sangat memuaskan!",
     link: "https://rog.asus.com/id/laptops-group/",
   },
   {
-    title: "Motherboard",
+    title: "Motherboards",
     name: "Alex Widjaja",
     review:
       "Sebagai seorang penggemar PC gaming, saya menginginkan motherboard yang bisa mendukung performa maksimal. Motherboard dari ASUS ROG benar-benar mengubah cara saya bermain. Stabilitasnya luar biasa, dan saya merasa pengalaman bermain game semakin halus tanpa ada lag. Desain dan fitur-fiturnya sungguh premium!",
     link: "https://rog.asus.com/id/motherboards-group/",
   },
   {
-    title: "Graphing Card",
+    title: "Graphics Cards",
     name: "Budi Setiawan",
     review:
       "Graphic card dari ROG benar-benar memberikan performa visual yang menakjubkan. Semua game yang saya mainkan berjalan dengan lancar pada pengaturan tertinggi, dan suhu tetap stabil berkat sistem pendinginnya yang canggih. Sangat direkomendasikan untuk gamer hardcore!",
     link: "https://rog.asus.com/id/graphics-cards-group/",
   },
   {
-    title: "Monitor",
+    title: "Monitors",
     name: "Rina Susanti",
     review:
       "Monitor ROG memberikan kejernihan yang luar biasa dengan refresh rate tinggi. Warna-warna tampak hidup, dan pengalaman bermain game menjadi lebih immersive. Desainnya yang ergonomis juga membuat sesi bermain panjang jadi lebih nyaman.",
     link: "https://rog.asus.com/id/monitors-group/",
   },
   {
-    title: "Phone",
+    title: "Phones",
     name: "Andi Pratama",
     review:
       "ROG Phone adalah perangkat terbaik untuk mobile gaming. Performa cepat, baterai tahan lama, dan sistem pendingin yang efektif membuatnya unggul dibandingkan ponsel lain. Pengalaman gaming tidak pernah semenyenangkan ini!",
     link: "https://rog.asus.com/id/phones-group/allmodels/  ",
   },
   {
-    title: "Desktop",
+    title: "Desktops",
     name: "Maria Santoso",
     review:
       "Desktop ROG ini memiliki kekuatan komputasi yang mengesankan. Semua aplikasi dan game berat bisa dijalankan tanpa hambatan. Saya juga suka desainnya yang futuristik dan pencahayaan RGB-nya.",
     link: "https://rog.asus.com/id/desktops-group/",
   },
   {
-    title: "Power Supply Unit",
+    title: "Power Supply Units",
     name: "Arif Wibowo",
     review:
       "PSU dari ROG sangat andal dan memberikan pasokan daya yang stabil. Saya merasa aman menjalankan sistem saya dengan komponen yang memerlukan daya besar. Plus, efisiensi energinya membantu menghemat biaya listrik.",
     link: "https://rog.asus.com/id/power-supply-units-group/",
   },
   {
-    title: "External Graphic Dock",
+    title: "External Graphic Docks",
     name: "Lili Setyawati",
     review:
       "This is the best laptop gaming i've ever experiencedExternal graphic dock dari ROG ini mengubah laptop biasa saya menjadi mesin gaming yang tangguh. Koneksi mudah dan performanya mengesankan. Sangat cocok untuk para pekerja yang ingin sesekali bermain game serius.",
@@ -63,49 +63,49 @@ export const dataDetailedPage = [
     link: "https://rog.asus.com/id/networking-group/allmodels/",
   },
   {
-    title: "Keyboard",
+    title: "Keyboards",
     name: "Surya Raharja",
     review:
       "Keyboard gaming ROG sangat responsif dengan backlit RGB yang dapat dikustomisasi. Keycaps-nya nyaman untuk sesi gaming panjang maupun mengetik. Benar-benar kombinasi sempurna untuk performa dan estetika.",
     link: "https://rog.asus.com/id/keyboards-group/",
   },
   {
-    title: "Mice and Mouse Pad",
+    title: "Mice & Mouse Pads",
     name: "Fitri Kusuma",
     review:
       "Mouse ROG ini sangat presisi dan ergonomis, sementara mouse pad-nya memberikan permukaan halus yang memudahkan pergerakan. Keduanya meningkatkan performa gaming saya secara signifikan.",
     link: "https://rog.asus.com/id/mice-mouse-pads-group/",
   },
   {
-    title: "Headset and Audio",
+    title: "Headsets & Audio",
     name: "Riko Jatmiko",
     review:
       "Headset ROG menghadirkan suara yang jernih dengan bass mendalam. Mikrofon juga jelas dan cocok untuk komunikasi selama game. Sesi gaming jadi lebih seru dan immersive.",
     link: "https://rog.asus.com/id/headsets-audio-group/",
   },
   {
-    title: "Case",
+    title: "Cases",
     name: "Dani Wijaya",
     review:
       "Casing ROG ini punya desain yang keren dan sirkulasi udara yang optimal. Pemasangan komponen mudah dan semua tetap dingin, bahkan saat digunakan intensif. Sungguh investasi yang bagus untuk build PC.",
     link: "https://rog.asus.com/id/cases-group/allmodels/",
   },
   {
-    title: "Apparel Bag and Gear",
+    title: "Apparel, Bags, & Gear",
     name: "Tia Putri",
     review:
       "Tas ROG ini sangat stylish dan praktis. Banyak kantong untuk menyimpan perlengkapan gaming dan laptop saya dengan aman. Materialnya juga kuat dan tahan lama.",
     link: "https://rog.asus.com/id/apparel-bags-gear-group/",
   },
   {
-    title: "Streaming Kit",
+    title: "Streaming Kits",
     name: "Anton Gunawan",
     review:
       "Streaming kit dari ROG sangat membantu untuk meningkatkan kualitas siaran saya. Kamera dan mikrofon bekerja sangat baik, dan setup-nya sangat mudah. Sungguh solusi lengkap untuk streamer pemula maupun profesional.",
     link: "https://rog.asus.com/id/streaming-kits-group/allmodels/",
   },
   {
-    title: "Controller",
+    title: "Controllers",
     name: "Bayu Saputra",
     review:
       "Controller ROG ini sangat nyaman digenggam dan responsif. Semua tombol terasa presisi, membuat pengalaman bermain di PC dan konsol jadi lebih menyenangkan.",
@@ -119,7 +119,7 @@ export const dataDetailedPage = [
     link: "https://rog.asus.com/storage-group/allmodels/",
   },
   {
-    title: "Charger, Adapter & Phone Accessories",
+    title: "Charger, Adapter, & Phone Accessories",
     name: "Eko Priyono",
     review:
       "Aksesori pengisi daya ROG bekerja sangat cepat dan efisien. Tidak perlu khawatir baterai habis di tengah gaming. Sangat andal dan tahan lama!",
@@ -133,7 +133,7 @@ export const dataDetailedPage = [
     link: "https://rog.asus.com/id/cooling-group/",
   },
   {
-    title: "Gaming Handheld",
+    title: "Gaming Handhelds",
     name: "Rian Suryadi",
     review:
       "Gaming handheld dari ROG memberikan pengalaman bermain setara konsol di mana saja. Layar yang jernih dan performa baterai yang bagus membuat saya bisa bermain lebih lama dengan nyaman.",
